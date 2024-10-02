a1bacb0ae1d7cc9e268917097388ff240c9ccdf5
Sibyl-null <1286438810@qq.com>
2024-10-02 23:33:05 +0800

    [feat]: 角色碰到就销毁

--- SurvivorX-Unity/Assets/Scripts/Player.cs
+++ SurvivorX-Unity/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using QFramework;
 using UnityEngine;
 
 namespace SurvivorX
@@ -5,10 +6,14 @@ namespace SurvivorX
     public class Player : MonoBehaviour
     {
         private Transform _transform;
+        [SerializeField] private Collider2D _hurtBox;
         
         private void Awake()
         {
             _transform = transform;
+
+            _hurtBox.OnTriggerEnter2DEvent(OnHurtBoxEnter)
+                .UnRegisterWhenGameObjectDestroyed(gameObject);
         }
 
         private void Update()
@@ -19,6 +24,11 @@ namespace SurvivorX
             Vector2 direction = new Vector2(horizontalInput, verticalInput).normalized;
             Vector2 movement = direction * Time.deltaTime * 5;
             _transform.position = (Vector2)_transform.position + movement;
+        }
+        
+        private void OnHurtBoxEnter(Collider2D box)
+        {
+            Destroy(gameObject);
         }
     }
 }
