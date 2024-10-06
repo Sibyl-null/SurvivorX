4b7f29bb6fe6807d3baf6a638c6a6fd6c2a106c2
Sibyl-null <1286438810@qq.com>
2024-10-06 20:39:56 +0800

    [feat]: 敌人受击

---
+++ SurvivorX-Unity/Assets/Scripts/Abilities/SimpleAbility.cs
@@ -0,0 +1,44 @@
+﻿using SurvivorX.Actors;
+using UnityEngine;
+
+namespace SurvivorX.Abilities
+{
+    public class SimpleAbility : MonoBehaviour
+    {
+        private Transform _transform;
+        private float _timeDown;
+
+        private void Awake()
+        {
+            _timeDown = 1.5f;
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _timeDown -= Time.deltaTime;
+            if (_timeDown <= 0)
+            {
+                _timeDown = 1.5f;
+                HurtAllEnemies();
+            }
+        }
+
+        private void HurtAllEnemies()
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Enemy cp = enemy.GetComponent<Enemy>();
+                if (cp == null)
+                    continue;
+                
+                float distance = Vector3.Distance(_transform.position, enemy.transform.position);
+                if (distance > 5f)
+                    continue;
+                
+                cp.BeHurt().Forget();
+            }
+        }
+    }
+}
--- SurvivorX-Unity/Assets/Scripts/Actors/Enemy.cs
+++ SurvivorX-Unity/Assets/Scripts/Actors/Enemy.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace SurvivorX.Actors
 {
     public class Enemy : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer _spRenderer; 
+        
         private Transform _targetTrans;
         private Transform _transform;
 
@@ -21,6 +24,13 @@ namespace SurvivorX.Actors
             Vector2 direction = (_targetTrans.position - _transform.position).normalized;
             Vector2 movement = direction * Time.deltaTime * 2;
             _transform.position = (Vector2)_transform.position + movement;
+        }
+
+        public async UniTaskVoid BeHurt()
+        {
+            _spRenderer.color = Color.red;
+            await UniTask.Delay(300, cancellationToken: this.GetCancellationTokenOnDestroy());
+            _spRenderer.color = Color.white;
         }
     }
 }
