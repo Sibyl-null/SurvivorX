df3a006be4265b3042d0bfcd33b4805a6ed02bc4
Sibyl-null <1286438810@qq.com>
2024-10-20 00:00:51 +0800

    [feat]: Enemy 使用配置的 Atk

--- SurvivorX-Unity/Assets/Scripts/Battle/Enemy/EnemyLifetimeScope.cs
+++ SurvivorX-Unity/Assets/Scripts/Battle/Enemy/EnemyLifetimeScope.cs
@@ -1,4 +1,5 @@
 using SurvivorX.Battle.Enemy.Move;
+using SurvivorX.Battle.Skills;
 using SurvivorX.Config;
 using UnityEngine;
 using VContainer;
@@ -10,12 +11,20 @@ namespace SurvivorX.Battle.Enemy
     {
         [SerializeField] private EnemyStatConfigSo _enemyStat;
         [SerializeField] private EnemyCharacter _enemyCharacter;
+        [SerializeField] private TouchSkill _touchSkill;
         
         protected override void Configure(IContainerBuilder builder)
         {
             builder.RegisterInstance(_enemyStat);
             builder.RegisterComponent(_enemyCharacter).AsImplementedInterfaces();
             builder.RegisterEntryPoint<EnemyMoveHandler>(Lifetime.Scoped);
+
+            SkillsInit();
+        }
+
+        private void SkillsInit()
+        {
+            _touchSkill.Init(_enemyStat.Atk);
         }
     }
 }
--- SurvivorX-Unity/Assets/Scripts/Battle/Skills/TouchSkill.cs
+++ SurvivorX-Unity/Assets/Scripts/Battle/Skills/TouchSkill.cs
@@ -5,13 +5,20 @@ namespace SurvivorX.Battle.Skills
 {
     public class TouchSkill : MonoBehaviour
     {
+        private int Atk { get; set; }
+
+        public void Init(int atk)
+        {
+            Atk = atk;
+        }
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             HurtBox hurtBox = other.GetComponent<HurtBox>();
             if (hurtBox == null)
                 return;
 
-            hurtBox.BeHit(1);
+            hurtBox.BeHit(Atk);
         }
     }
 }
